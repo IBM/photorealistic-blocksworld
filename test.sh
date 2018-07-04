09000d592ebeb3c07e4db5134350c09812d9bce5
Masataro Asai <guicho2.71828@gmail.com>
2018-07-04 17:14:37 +0900

    faster test

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 blender -noaudio \
         --background --python render_images.py -- \
-        --num-images 10 \
-        --render-num-samples 40 \
-        --width 300 \
-        --height 200 \
+        --num-images 1 \
+        --render-num-samples 100 \
+        --width 600 \
+        --height 400 \
         --num-objects 10 \
         --max-stacks 6
 
