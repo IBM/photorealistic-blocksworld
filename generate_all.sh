e442cf4ab9f1343d5e35af6cd13dc28bd09e7466
Masataro Asai <Masataro.Asai@ibm.com>
2018-08-23 18:14:02 +0900

    smaller fig, more objects

--- generate_all.sh
+++ generate_all.sh
@@ -3,13 +3,12 @@
 blender -noaudio \
         --background --python render_all_images.py -- \
         --render-num-samples 200 \
-        --output-scene-dir output/scenes/ \
-        --output-image-dir output/images/ \
-        --output-scene-file output/scene.json \
-        --width 300 \
-        --height 200 \
-        --num-objects 3 \
-        --max-stacks 4
+        --output-dir output-4-4 \
+        --width 150 \
+        --height 100 \
+        --num-objects 4 \
+        --max-stacks 4 \
+        --use-gpu 1
 
 # parallel ./extract_region.py ::: output/scenes/*.json
 
