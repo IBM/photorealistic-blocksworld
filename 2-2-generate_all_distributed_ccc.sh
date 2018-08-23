3c36970ff8c9fafacc53f985b8cce6d6c65cfb72
Masataro Asai Masataro.Asai@ibm.com <jp576066@dccxl016.pok.ibm.com>
2018-08-23 11:18:06 -0400

    test with 2-2

---
+++ 2-2-generate_all_distributed_ccc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+common="jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $(date -Iminutes)"
+# dir=$(dirname $(dirname $(readlink -ef $0)))
+# export PYTHONPATH=$dir:$PYTHONPATH
+# export PYTHONUNBUFFERED=1
+
+parallel "$common ./2-2-generate_all_distributed.sh {}" ::: $(seq 3)
+
