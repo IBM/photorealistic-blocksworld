b2cae8a25a9f60257226eec56ff4139264e9f0dd
Masataro Asai <guicho2.71828@gmail.com>
2018-09-28 15:36:14 +0900

    added readme to generate_all

--- generate_all.sh
+++ generate_all.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -x
 
+# Render all scenes with a given number of objects and stacks.
+#
+# generate_all.sh [objs] [stacks] [distributed] [num_images]
+#
+#   objs:   specity the number of objects, default = 2
+# 
+#   stacks: specity the number of stacks,  default = 2
+# 
+#   distributed: Whether to split the jobs and run the rendering in parallel. true|false. default = false
+# 
+#   num_images:  The number of images per job when distributed=true.
+#
+# You can modity the "submit" variable in the source code to
+# customize the job submission commands for the job scheduler in your cluster.
+
 objs=${1:-2}
 stacks=${2:-2}
 distributed=${3:-false}
 num_images=${4:-200}
 prefix="blocks-$objs-$stacks"
 proj=$(date +%Y%m%d%H%M)-render-$prefix
-jbsub="jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj"
+
+submit="jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj"
 
 blender="blender -noaudio --background --python render_all_images.py -- \
       --output-dir      $prefix                   \
@@ -25,8 +41,8 @@ transitions=$(jq .transitions $prefix-stat.json)
 
 if $distributed
 then
-    parallel "$jbsub $blender --use-gpu 1 --start-idx {} --num-images $num_images" ::: $(seq 0 $num_images $states)
-    ccc/watch-proj $proj && $jbsub "./extract_all_regions_binary.py --out $prefix.npz $prefix/"
+    parallel "$submit $blender --use-gpu 1 --start-idx {} --num-images $num_images" ::: $(seq 0 $num_images $states)
+    ccc/watch-proj $proj && $submit "./extract_all_regions_binary.py --out $prefix.npz $prefix/"
 else
-    $jbsub "$blender --use-gpu 1 ; ./extract_all_regions_binary.py --out $prefix.npz $prefix/"
+    $submit "$blender --use-gpu 1 ; ./extract_all_regions_binary.py --out $prefix.npz $prefix/"
 fi
