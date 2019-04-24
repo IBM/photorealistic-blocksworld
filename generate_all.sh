0e25c3d230aa3c37e4471d3c351d33f22028cb4f
Masataro Asai <guicho2.71828@gmail.com>
2019-04-24 18:33:25 +0900

    generate-all.sh

--- generate_all.sh
+++ generate_all.sh
@@ -15,36 +15,37 @@
 # You can modity the "submit" variable in the source code to
 # customize the job submission commands for the job scheduler in your cluster.
 
-objs=${1:-2}
-stacks=${2:-2}
+min=${1:-3}
+max=${2:-8}
 distributed=${3:-false}
 num_images=${4:-200}
-prefix="blocks-$objs-$stacks"
+gpu=${5:true}
+prefix="clevr-$min-$max"
 proj=$(date +%Y%m%d%H%M)-render-$prefix
+
+if $gpu
+then
+    gpuflag="--use-gpu 1"
+else
+    gpuflag=""
+fi
 
 submit="jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj"
 
 blender="blender -noaudio --background --python render_images.py -- \
       --output-dir      $prefix                   \
-      --initial-objects $prefix-init.json                \
-      --statistics      $prefix-stat.json                \
       --render-num-samples 300                           \
       --width 300                                        \
       --height 200                                       \
-      --num-objects $objs                                \
-      --max-stacks $stacks                               "
-
-$blender --dry-run || exit 1      # necessary for init-o-s.json
-
-states=$(jq      .states      $prefix-stat.json)
-transitions=$(jq .transitions $prefix-stat.json)
+      --min-objects $min                                
+      --max-objects $max                                "
 
 if $distributed
 then
-    parallel "$submit $blender --use-gpu 1 --start-idx {} --num-images $num_images" ::: $(seq 0 $num_images $states)
+    parallel "$submit $blender $gpuflag --start-idx {} --num-images $num_images" ::: $(seq 0 $num_images $states)
     echo "Run the following command when all jobs have finished:"
     echo "./extract_all_regions_binary.py --out $prefix.npz $prefix/"
 else
-    $blender --use-gpu 1
+    $blender $gpuflag
     ./extract_all_regions_binary.py --out $prefix.npz $prefix/
 fi
