8cb84cef663e5a34fb3267a3987450a14c1f3685
Masataro Asai <guicho2.71828@gmail.com>
2019-04-24 18:28:10 +0900

    examples

--- test.sh
+++ test.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-echo "this test will just render the first 5 images of 2 objs, 2 stacks blocksworld."
-read -n 1 -s -r -p "Press any key to continue"
+blender --background -noaudio --python render_images.py -- \
+        --num-images 10 \
+        --width 300 \
+        --height 200 \
+        --render-num-samples 20 \
+        --max-objects 4
 
-blender -noaudio --background --python render_images.py -- \
-      --output-dir      output                          \
-      --initial-objects output-init.json                \
-      --statistics      output-stat.json                \
-      --render-num-samples 50                           \
-      --num-images         5                            \
-      --start-idx          0                            \
-      --width 300                                       \
-      --height 200                                      \
-      --num-objects 2                                   \
-      --max-stacks  2
-
-
-# ./extract_region.py output/scenes/CLEVR_new_000000_pre.json
-# ./extract_region.py output/scenes/CLEVR_new_000000_suc.json
+./extract_region.py output/scene/CLEVR_new_000000.json
