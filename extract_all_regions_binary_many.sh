55c9ea5c7942d26985d185232d5b699fd8cf24ae
Masataro Asai <guicho2.71828@gmail.com>
2020-09-02 15:11:27 -0400

    fix typo: npz

--- extract_all_regions_binary_many.sh
+++ extract_all_regions_binary_many.sh
@@ -5,5 +5,5 @@ dir=blocks-3-3-multi
 
 parallel -j 16 ./extract_all_regions_binary.py --out $dir/{}.npz $dir/{} ::: {1..100}
 
-./merge-npz.py --out $dir/$dir.npx $dir/{1..100}.npz
+./merge-npz.py --out $dir/$dir.npz $dir/{1..100}.npz
 
