1e66b73ff2da30c25d5bb0d0d0d82915b3b2338c
Masataro Asai <guicho2.71828@gmail.com>
2020-09-21 18:21:56 -0400

    remove unused shell scripts

--- dump_binary_many.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-
-dir=blocks-3-3-multi
-
-parallel -j 32 ./dump_binary.py --out $dir/{}-globalonly.npz $dir/{} ::: {1..100}
-
-./merge-npz.py --out $dir/$dir-globalonly.npz $dir/{1..100}-globalonly.npz
-
