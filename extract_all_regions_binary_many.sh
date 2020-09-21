01ccb996146dd22b8c0f13d3e03e2bb62a8d77cd
Masataro Asai <guicho2.71828@gmail.com>
2020-09-21 18:21:56 -0400

    remove unused shell scripts

--- extract_all_regions_binary_many.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-
-dir=blocks-3-3-multi
-
-parallel -j 16 ./extract_all_regions_binary.py --out $dir/{}.npz $dir/{} ::: {1..100}
-
-./merge-npz.py --out $dir/$dir.npz $dir/{1..100}.npz
-
--- generate_all_many.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# generate blocks-3-3 with 100 different random colors and shapes
-
-parallel -j 1 ./generate_all.sh 3 true 200 true /{} ::: {1..100}
