91e230dac09bec1037cc122c5bc0725f739b8ec6
Masataro Asai <guicho2.71828@gmail.com>
2020-12-28 20:02:01 -0500

    generate_problems: when invoked without arguments, print the help message and exit

--- generate_problems.sh
+++ generate_problems.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-# Render a pair of scenes, where the second one is a shuffled state.
-# It also runs extract_all_regions_binary.
+if [ -z $@ ]
+then
 
-# The pair can be seen as an initial and the goal configuration of a planning task.
+    cat <<EOF >&2
 
-# generate_problems.sh [objs] [steps] [num_problems] [gpu] [suffix]
-#
-#   objs:   specity the number of objects, default = 2
-#
-#   steps:   specity the number of random actions to perform, default = 4
-# 
-#   num_problems:  The number of problems to be rendered in total.
-#
-#   gpu:  if true, use the gpu. default : true.
-#
-#   suffix:  arbitrary string to be attached to the name of the output directory.
-# 
-#
-# use it like: parallel --line-buffer ./generate_problems.sh {} 10 ::: 3 4 5 6 ::: 1 2 3 5 8 13 21
+
+    Usage: generate_problems.sh [objs] [steps] [num_problems] [gpu] [suffix]
+
+      objs:   specity the number of objects, default = 2
+
+      steps:   specity the number of random actions to perform, default = 4
+
+      num_problems:  The number of problems to be rendered in total.
+
+      gpu:  if true, use the gpu. default : true.
+
+      suffix:  arbitrary string to be attached to the name of the output directory.
+
+
+    Render a pair of scenes, where the second one is a shuffled state.
+    The pair can be seen as an initial and the goal configuration of a planning task.
+    It also runs extract_all_regions_binary.
+
+    Use it like this: parallel --line-buffer ./generate_problems.sh {} 10 ::: 3 4 5 6 ::: 1 2 3 5 8 13 21
+
+EOF
+    exit 1
+fi
 
 export objs=${1:-3}
 export steps=${2:-4}
