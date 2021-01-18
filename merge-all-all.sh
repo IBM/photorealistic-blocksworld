f7dbc74bcfbe030b4fd4ba289e6771c5a8d8de38
Masataro Asai <guicho2.71828@gmail.com>
2021-01-18 15:44:59 -0500

    merge-all-all: also merge flat

--- merge-all-all.sh
+++ merge-all-all.sh
@@ -2,4 +2,4 @@
 
 
 
-parallel --line-buffer -v ./merge-npz.py --out blocks-{1}-{2}.npz blocks-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd
+parallel --line-buffer -v ./merge-npz.py --out blocks-{1}-{2}.npz blocks-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat
