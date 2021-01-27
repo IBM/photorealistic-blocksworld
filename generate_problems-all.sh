1075c4267e17cfcb5961bf4a797a52c0b990be81
Masataro Asai <guicho2.71828@gmail.com>
2021-01-27 18:48:40 -0500

    generate_problems-all.sh

---
+++ generate_problems-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export proj=$(date +%Y%m%d%H%M)-render-problems
+SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj"}
+
+parallel $SUBMIT ./generate_problems.sh ::: 3 4 5 6 ::: 1 2 3 5 8 13 21
