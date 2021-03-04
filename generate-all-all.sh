c02a6cf02b7388e0d6b8275e318f55c65a8a3979
Masataro Asai <guicho2.71828@gmail.com>
2021-03-04 11:10:30 -0500

    merge generate-all-all and merge-all-all

--- generate-all-all.sh
+++ generate-all-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -e
 
 parallel ./generate_all.sh {} 30000 50 ::: {3..6}
 
-list-proj
+echo render-monoblocks-{3..6}
+watch-proj render-monoblocks-{3..6}
+
+parallel --line-buffer -v ./merge-npz.py --out monoblocks-{1}-{2}.npz monoblocks-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
--- merge-all-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-
-parallel --line-buffer -v ./merge-npz.py --out monoblocks-{1}-{2}.npz monoblocks-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
