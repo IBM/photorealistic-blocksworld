fc61c8cf4cd6e12132ae06e79932a3e8086bfa36
Masataro Asai <guicho2.71828@gmail.com>
2021-05-04 10:35:13 -0400

    colored cylinders configuration: large only, colored

--- generate-all-all.sh
+++ generate-all-all.sh
@@ -4,7 +4,7 @@ set -e
 
 parallel ./generate_all.sh {} 40000 50 ::: {3..6}
 
-echo render-monoblocks-{3..6}
-watch-proj render-monoblocks-{3..6}
+echo render-cylinders-{3..6}
+watch-proj render-cylinders-{3..6}
 
-parallel --line-buffer -v ./merge-npz.py --out monoblocks-{1}-{2}.npz monoblocks-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
+parallel --line-buffer -v ./merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
