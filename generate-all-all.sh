b740864a17f75d14992b046fbbf50ec4371b42f9
Masataro Asai <guicho2.71828@gmail.com>
2021-05-05 10:47:38 -0400

    allow running the scripts from a different directory

--- generate-all-all.sh
+++ generate-all-all.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-parallel ./generate_all.sh {} 40000 50 ::: {3..6}
+dir=$(readlink -ef $(dirname $0))
+
+parallel $dir/generate_all.sh {} 40000 50 ::: {3..6}
 
 echo render-cylinders-{3..6}
 watch-proj render-cylinders-{3..6}
 
-parallel --line-buffer -v ./merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
+parallel --line-buffer -v $dir/merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
--- generate_problems-all.sh
+++ generate_problems-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+dir=$(readlink -ef $(dirname $0))
+
 export proj=$(date +%Y%m%d%H%M)-render-problems
 SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj -require k80"}
 
-parallel --line-buffer $SUBMIT ./generate_problems.sh ::: 3 4 5 6 ::: 1 2 3 5 8 13 21
+parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..6} ::: 1 2 3 5 8 13 21
