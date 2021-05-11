f2c41a533cc58a47f2fcf29d4d75d9eb147d0d64
Masataro Asai <guicho2.71828@gmail.com>
2021-05-10 21:43:34 -0400

    change problem parameters

--- generate_problems-all.sh
+++ generate_problems-all.sh
@@ -5,4 +5,5 @@ dir=$(readlink -ef $(dirname $0))
 export proj=$(date +%Y%m%d%H%M)-render-problems
 SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj -require k80"}
 
-parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..6} ::: 001 002 003 005 008 013 021
+# 1,2,3 are for debugging ; 7 and 14 are for final experiments
+parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..6} ::: 007 014
