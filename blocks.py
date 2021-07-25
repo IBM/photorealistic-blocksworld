2e01d1a9df30acf56704fef1e3b87c79f211b00f
Masataro Asai <guicho2.71828@gmail.com>
2021-07-25 16:34:16 -0400

    refactored initialize_parser

--- blocks.py
+++ blocks.py
@@ -1,5 +1,6 @@
 import json
 import random
+from datetime import datetime as dt
 
 properties         = {}
 
@@ -20,6 +21,89 @@ def load_colors(args):
 
   return
 
+
+
+def initialize_parser_input_options(parser):
+  # Input options
+  parser.add_argument('--base-scene-blendfile', default='data/base_scene.blend',
+                      help="Base blender file on which all scenes are based; includes " +
+                      "ground plane, lights, and camera.")
+  parser.add_argument('--properties-json', default='data/properties.json',
+                      help="JSON file defining objects, materials, sizes, and colors. " +
+                      "The \"colors\" field maps from CLEVR color names to RGB values; " +
+                      "The \"sizes\" field maps from CLEVR size names to scalars used to " +
+                      "rescale object models; the \"materials\" and \"shapes\" fields map " +
+                      "from CLEVR material and shape names to .blend files in the " +
+                      "--object-material-dir and --shape-dir directories respectively.")
+  parser.add_argument('--shape-dir', default='data/shapes',
+                      help="Directory where .blend files for object models are stored")
+  parser.add_argument('--material-dir', default='data/materials',
+                      help="Directory where .blend files for materials are stored")
+  parser.add_argument('--allow-duplicates', action="store_true",
+                      help="Allow duplicate objects")
+
+
+def initialize_parser_environment_options(parser):
+  # Settings for objects
+  parser.add_argument('--num-objects', default=4, type=int,
+                      help="The number of objects to place in each scene")
+
+  parser.add_argument('--table-size', default=5, type=int,
+                      help="The approximate table size relative to the large object size * 1.5.")
+  
+  parser.add_argument('--object-jitter', default=0.0, type=int,
+                      help="The magnitude of random jitter to add to the x,y position of each block.")
+
+def initialize_parser_output_options(parser,prefix):
+  parser.add_argument('--filename-prefix', default=prefix,
+                      help="This prefix will be prepended to the rendered images and JSON scenes")
+  parser.add_argument('--output-dir', default='output',
+                      help="The directory where output will be stored. It will be " +
+                      "created if it does not exist.")
+  parser.add_argument('--save-blendfiles', type=int, default=0,
+                      help="Setting --save-blendfiles 1 will cause the blender scene file for " +
+                      "each generated image to be stored in the directory specified by " +
+                      "the --output-blend-dir flag. These files are not saved by default " +
+                      "because they take up ~5-10MB each.")
+  parser.add_argument('--version', default='1.0',
+                      help="String to store in the \"version\" field of the generated JSON file")
+  parser.add_argument('--license',
+                      default="Creative Commons Attribution (CC-BY 4.0)",
+                      help="String to store in the \"license\" field of the generated JSON file")
+  parser.add_argument('--date', default=dt.today().strftime("%m/%d/%Y"),
+                      help="String to store in the \"date\" field of the generated JSON file; " +
+                      "defaults to today's date")
+
+def initialize_parser_rendering_options(parser):
+  # Rendering options
+  parser.add_argument('--use-gpu', default=0, type=int,
+                      help="Setting --use-gpu 1 enables GPU-accelerated rendering using CUDA. " +
+                      "You must have an NVIDIA GPU with the CUDA toolkit installed for " +
+                      "to work.")
+  parser.add_argument('--width', default=320, type=int,
+                      help="The width (in pixels) for the rendered images")
+  parser.add_argument('--height', default=240, type=int,
+                      help="The height (in pixels) for the rendered images")
+  parser.add_argument('--key-light-jitter', default=0.0, type=float,
+                      help="The magnitude of random jitter to add to the key light position.")
+  parser.add_argument('--fill-light-jitter', default=0.0, type=float,
+                      help="The magnitude of random jitter to add to the fill light position.")
+  parser.add_argument('--back-light-jitter', default=0.0, type=float,
+                      help="The magnitude of random jitter to add to the back light position.")
+  parser.add_argument('--camera-jitter', default=0.0, type=float,
+                      help="The magnitude of random jitter to add to the camera position")
+  parser.add_argument('--render-num-samples', default=512, type=int,
+                      help="The number of samples to use when rendering. Larger values will " +
+                      "result in nicer images but will cause rendering to take longer.")
+  parser.add_argument('--render-min-bounces', default=8, type=int,
+                      help="The minimum number of bounces to use for rendering.")
+  parser.add_argument('--render-max-bounces', default=8, type=int,
+                      help="The maximum number of bounces to use for rendering.")
+  parser.add_argument('--render-tile-size', default=256, type=int,
+                      help="The tile size to use for rendering. This should not affect the " +
+                      "quality of the rendered image but may affect the speed; CPU-based " +
+                      "rendering may achieve better performance using smaller tile sizes " +
+                      "while larger tile sizes may be optimal for GPU-based rendering.")
 
 
 def random_dict(dict):
--- render_images.py
+++ render_images.py
@@ -7,7 +7,6 @@
 
 from __future__ import print_function
 import sys, argparse, json, os
-from datetime import datetime as dt
   
 INSIDE_BLENDER = True
 try:
@@ -35,34 +34,14 @@ def initialize_parser():
   parser = argparse.ArgumentParser()
 
   # Input options
-  parser.add_argument('--base-scene-blendfile', default='data/base_scene.blend',
-                      help="Base blender file on which all scenes are based; includes " +
-                      "ground plane, lights, and camera.")
-  parser.add_argument('--properties-json', default='data/properties.json',
-                      help="JSON file defining objects, materials, sizes, and colors. " +
-                      "The \"colors\" field maps from CLEVR color names to RGB values; " +
-                      "The \"sizes\" field maps from CLEVR size names to scalars used to " +
-                      "rescale object models; the \"materials\" and \"shapes\" fields map " +
-                      "from CLEVR material and shape names to .blend files in the " +
-                      "--object-material-dir and --shape-dir directories respectively.")
-  parser.add_argument('--shape-dir', default='data/shapes',
-                      help="Directory where .blend files for object models are stored")
-  parser.add_argument('--material-dir', default='data/materials',
-                      help="Directory where .blend files for materials are stored")
-  parser.add_argument('--allow-duplicates', action="store_true",
-                      help="Allow duplicate objects")
-  
-  # Settings for objects
-  parser.add_argument('--num-objects', default=4, type=int,
-                      help="The number of objects to place in each scene")
+  blocks.initialize_parser_input_options(parser)
 
-  parser.add_argument('--table-size', default=5, type=int,
-                      help="The approximate table size relative to the large object size * 1.5.")
-  
-  parser.add_argument('--object-jitter', default=0.0, type=int,
-                      help="The magnitude of random jitter to add to the x,y position of each block.")
+  # Environment options
+  blocks.initialize_parser_environment_options(parser)
   
   # Output settings
+  blocks.initialize_parser_output_options(parser,prefix='CLEVR')
+
   parser.add_argument('--start-idx', default=0, type=int,
                       help="The index at which to start for numbering rendered images. Setting " +
                       "this to non-zero values allows you to distribute rendering across " +
@@ -70,54 +49,10 @@ def initialize_parser():
   parser.add_argument('--num-images', default=100, type=int,
                       help="The number of images to render")
   
-  parser.add_argument('--filename-prefix', default='CLEVR',
-                      help="This prefix will be prepended to the rendered images and JSON scenes")
-  parser.add_argument('--output-dir', default='output',
-                      help="The directory where output will be stored. It will be " +
-                      "created if it does not exist.")
-  parser.add_argument('--save-blendfiles', type=int, default=0,
-                      help="Setting --save-blendfiles 1 will cause the blender scene file for " +
-                      "each generated image to be stored in the directory specified by " +
-                      "the --output-blend-dir flag. These files are not saved by default " +
-                      "because they take up ~5-10MB each.")
-  parser.add_argument('--version', default='1.0',
-                      help="String to store in the \"version\" field of the generated JSON file")
-  parser.add_argument('--license',
-                      default="Creative Commons Attribution (CC-BY 4.0)",
-                      help="String to store in the \"license\" field of the generated JSON file")
-  parser.add_argument('--date', default=dt.today().strftime("%m/%d/%Y"),
-                      help="String to store in the \"date\" field of the generated JSON file; " +
-                      "defaults to today's date")
   
   # Rendering options
-  parser.add_argument('--use-gpu', default=0, type=int,
-                      help="Setting --use-gpu 1 enables GPU-accelerated rendering using CUDA. " +
-                      "You must have an NVIDIA GPU with the CUDA toolkit installed for " +
-                      "to work.")
-  parser.add_argument('--width', default=320, type=int,
-                      help="The width (in pixels) for the rendered images")
-  parser.add_argument('--height', default=240, type=int,
-                      help="The height (in pixels) for the rendered images")
-  parser.add_argument('--key-light-jitter', default=0.0, type=float,
-                      help="The magnitude of random jitter to add to the key light position.")
-  parser.add_argument('--fill-light-jitter', default=0.0, type=float,
-                      help="The magnitude of random jitter to add to the fill light position.")
-  parser.add_argument('--back-light-jitter', default=0.0, type=float,
-                      help="The magnitude of random jitter to add to the back light position.")
-  parser.add_argument('--camera-jitter', default=0.0, type=float,
-                      help="The magnitude of random jitter to add to the camera position")
-  parser.add_argument('--render-num-samples', default=512, type=int,
-                      help="The number of samples to use when rendering. Larger values will " +
-                      "result in nicer images but will cause rendering to take longer.")
-  parser.add_argument('--render-min-bounces', default=8, type=int,
-                      help="The minimum number of bounces to use for rendering.")
-  parser.add_argument('--render-max-bounces', default=8, type=int,
-                      help="The maximum number of bounces to use for rendering.")
-  parser.add_argument('--render-tile-size', default=256, type=int,
-                      help="The tile size to use for rendering. This should not affect the " +
-                      "quality of the rendered image but may affect the speed; CPU-based " +
-                      "rendering may achieve better performance using smaller tile sizes " +
-                      "while larger tile sizes may be optimal for GPU-based rendering.")
+  blocks.initialize_parser_rendering_options(parser)
+
   return parser
 
 
