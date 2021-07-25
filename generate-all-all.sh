54f69576ed10d40f21286590bd094f6632f7e365
Masataro Asai <guicho2.71828@gmail.com>
2021-07-25 16:36:33 -0400

    render 3,4,5,6,7

--- generate-all-all.sh
+++ generate-all-all.sh
@@ -4,9 +4,9 @@ set -e
 
 dir=$(readlink -ef $(dirname $0))
 
-parallel $dir/generate_all.sh {} 40000 50 ::: {3..6}
+parallel $dir/generate_all.sh {} 40000 50 ::: {3..7}
 
-echo render-cylinders-{3..6}
-watch-proj render-cylinders-{3..6}
+echo render-cylinders-{3..7}
+watch-proj render-cylinders-{3..7}
 
-parallel --line-buffer -v $dir/merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..6} ::: objs bgnd flat high
+parallel --line-buffer -v $dir/merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..7} ::: objs bgnd flat high
--- generate_problems-all.sh
+++ generate_problems-all.sh
@@ -6,4 +6,4 @@ export proj=$(date +%Y%m%d%H%M)-render-problems
 SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj -require k80"}
 
 # 1,2,3 are for debugging ; 7 and 14 are for final experiments
-parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..6} ::: 007 014
+parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..7} ::: 007 014
