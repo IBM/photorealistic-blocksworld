1f33d67e129e5b8ad62960b0645dee9fc6759863
Masataro Asai <guicho2.71828@gmail.com>
2022-01-07 21:07:28 -0500

    remove k80 rquirements

--- generate_problems-all.sh
+++ generate_problems-all.sh
@@ -3,7 +3,7 @@
 dir=$(readlink -ef $(dirname $0))
 
 export proj=$(date +%Y%m%d%H%M)-render-problems
-SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj -require k80"}
+SUBMIT=${SUBMIT:-"jbsub -mem 4g -cores 1+1 -queue x86_1h -proj $proj"}
 
 # 1,2,3 are for debugging ; 7 and 14 are for final experiments
 parallel --line-buffer $SUBMIT $dir/generate_problems.sh ::: {3..7} ::: 001 002 003 007 014
