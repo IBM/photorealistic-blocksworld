d586f2e98ab6b4049b5b402133d5e833afb3dcb3
Masataro Asai <guicho2.71828@gmail.com>
2022-03-24 15:17:43 -0400

    limit parallelism

--- generate-all-all.sh
+++ generate-all-all.sh
@@ -9,4 +9,4 @@ parallel $dir/generate_all.sh {} 40000 3 50 ::: {3..7}
 echo render-cylinders-{3..7}
 watch-proj render-cylinders-{3..7}
 
-parallel --line-buffer -v $dir/merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..7} ::: objs bgnd flat high
+parallel -j 8 --line-buffer -v $dir/merge-npz.py --out cylinders-{1}-{2}.npz cylinders-{1}/*-{2}.npz ::: {3..7} ::: objs bgnd flat high
